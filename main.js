cf760e2e39e66367a7ebfd9933642693e8d2e7bb
Kappach <123407166+Kappach@users.noreply.github.com>
2023-02-15 17:52:45 +0200

    Add files via upload
    initial commit

---
+++ main.js
@@ -0,0 +1,60 @@
+/* 
+1
+var one = 5
+var two = 5
+
+if (one === two) {
+    console.log("Boom")
+}
+ else { console.log 
+    ("TRY AGAIN")   
+}
+*/
+
+/*
+ 2
+var one = 7
+var two = 8
+
+console.log (one + two)
+*/
+
+/* 
+3
+var one = 5
+var two = 5
+
+console.log(one * two)
+*/ 
+
+var sunday = 1
+var monday = 2
+var tuesday = 3
+var wednesday = 4
+var thursday = 5
+var friday = 6 
+var saturday = 7
+
+/* 
+4
+if (1 === sunday) {
+    console.log ("Good Week!")
+} else if (sunday < monday) {
+    console.log ("Good Day!") 
+}
+else if (sunday < tuesday) {
+    console.log ("Good Day!") 
+}
+else if (sunday < wednesday) {
+    console.log ("Good Day!") 
+}
+else if (sunday < 5) {
+    console.log ("Happy Weekend!") 
+}
+else if (sunday === 5) {
+    console.log ("Happy Weekend!") 
+}
+*/
+
+
+
